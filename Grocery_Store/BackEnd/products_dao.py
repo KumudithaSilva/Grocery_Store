0b876e74e5bb63041fe2cb38a91cd60fb9baf94b
kumuditha <kumudithasilva66@gmail.com.>
2023-11-26 22:15:31 +0530

    create inset, update and delete features

--- Grocery_Store/BackEnd/products_dao.py
+++ Grocery_Store/BackEnd/products_dao.py
@@ -20,10 +20,54 @@ def get_all_products(connection):
     return response
 
 
+def insert_products(connection, products):
+   
+   cursor = connection.cursor()
+   query = "INSERT INTO products (`name`, `uom_id`, `price_per_unit`) VALUES (%s, %s, %s);"
+
+   data = (products['name'], products['uom_id'], products['price_per_unit'])
+   cursor.execute(query, data)
+   connection.commit()
+
+
+def update_products(connection, products):
+   
+   cursor = connection.cursor()
+   query = "UPDATE products SET name=%s, uom_id=%s, price_per_unit=%s WHERE products_id=" + str(products['id'])
+
+   data = (products['name'], products['uom_id'], products['price_per_unit'])
+   cursor.execute(query, data)
+   connection.commit()
+
+
+def delete_products(connection, product_id):
+   
+   cursor = connection.cursor()
+   query = "DELETE FROM products WHERE products_id=" + str(product_id)
+
+   cursor.execute(query)
+   connection.commit()
+    
+
+
 if __name__ == '__main__':
     connection = get_sql_connection()
-    if connection:
-        try:
-            print(get_all_products(connection))
-        finally:
-            connection.close()
+    print(update_products(connection, {
+       'id': 4,
+       'name' : 'cabbage',
+       'uom_id' : '1',
+       'price_per_unit' : '10'
+    }))
+
+    #if connection:
+    #   try:
+    #        print(get_all_products(connection))
+    #    finally:
+    #        connection.close()
+
+    #(update_products(connection, {
+    #   'id': 4,
+    #   'name' : 'cabbage',
+    #   'uom_id' : '1',
+    #   'price_per_unit' : '100'
+    #}))
