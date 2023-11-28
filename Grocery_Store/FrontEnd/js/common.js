2f9c4afa6dec816f9120d7b6ae7d418e4e6584c7
kumuditha <kumudithasilva66@gmail.com.>
2023-11-28 17:09:47 +0530

    add separated API urls

--- Grocery_Store/FrontEnd/js/common.js
+++ Grocery_Store/FrontEnd/js/common.js
@@ -1,10 +1,1 @@
-function onPageLoad() {
-    console.log("document loaded");
-    var url = "http://127.0.0.1:5000/getproducts";
-    $.get(url, function(data, status){
-        console.log("got response for products");
-        console.log(data)
-    });
-}
-
-window.onload = onPageLoad;
+var productListApiUrl = "http://127.0.0.1:5000/getproducts";
---
+++ Grocery_Store/FrontEnd/js/manage-products.js
@@ -0,0 +1,16 @@
+var productModal = $("#productModal");
+    $(function () {
+        $.get(productListApiUrl, function (response) {
+            if(response) {
+                var table = '';
+                $.each(response, function(index, product) {
+                    table += '<tr data-id="'+ product.product_id +'" data-name="'+ product.name +'" data-unit="'+ product.uom_id +'" data-price="'+ product.price_per_unit +'">' +
+                        '<td>'+ product.name +'</td>'+
+                        '<td>'+ product.uom_name +'</td>'+
+                        '<td>'+ product.price_per_unit +'</td>'+
+                        '<td><span class="btn btn-xs btn-danger delete-product">Delete</span></td></tr>';
+                });
+                $("table").find('tbody').empty().html(table);
+            }
+        });
+    });
