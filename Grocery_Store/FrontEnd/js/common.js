7cee0792ca05bbdc81029468d0b8351f0d8836e7
kumuditha <kumudithasilva66@gmail.com.>
2023-11-29 23:42:35 +0530

    add uoms list url

--- Grocery_Store/FrontEnd/js/common.js
+++ Grocery_Store/FrontEnd/js/common.js
@@ -1,5 +1,6 @@
 var productListApiUrl = "http://127.0.0.1:5000/getproducts";
 var productDeleteApiUrl = "http://127.0.0.1:5000/deleteproducts";
+var uomListApiUrl = "http://127.0.0.1:5000/getuoms";
 
 function callApi(method, url, data) {
     $.ajax({
