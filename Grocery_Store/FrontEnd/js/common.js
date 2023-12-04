d32878f0a41c2ec2d51ed2ce81f824cf5b3cbbb5
kumuditha <kumudithasilva66@gmail.com.>
2023-12-04 23:50:34 +0530

    add update url

--- Grocery_Store/FrontEnd/js/common.js
+++ Grocery_Store/FrontEnd/js/common.js
@@ -1,5 +1,6 @@
 var productListApiUrl = "http://127.0.0.1:5000/getproducts";
 var productInsertApiUrl = "http://127.0.0.1:5000/insertproducts";
+var productUpdateApiUrl = "http://127.0.0.1:5000/updateproducts";
 var productDeleteApiUrl = "http://127.0.0.1:5000/deleteproducts";
 var uomListApiUrl = "http://127.0.0.1:5000/getuoms";
 
