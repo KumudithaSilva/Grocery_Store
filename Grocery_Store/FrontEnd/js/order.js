94a22eeaaa02e4feb69b9acb4407d7b65c5a25f1
kumuditha <kumudithasilva66@gmail.com.>
2023-12-15 20:58:05 +0530

    create order options

--- Grocery_Store/FrontEnd/js/order.js
+++ Grocery_Store/FrontEnd/js/order.js
@@ -15,3 +15,31 @@ $(document).on("click", '.remove-row', function(){
     $(this).closest('.row').remove();
 });
 
+
+$(function(){
+    $.get(productListApiUrl, function(response){
+        if(response){
+            products_prices = {}
+            var options = '<option value="">--Select--</options>'
+            $.each(response, function(index, products){
+                options += '<option value=" '+products.products_id + '">'+ products.name + '</options>';
+                products_prices[products.products_id]= products.price_per_unit
+            });
+
+        }
+        $('.product-box').find("select").empty().html(options);
+
+    });
+});
+
+
+$(document).on("change", ".cart-product", function(){
+    product_id = $(this).val()
+    console.log(product_id)
+
+    product_price = products_prices[parseInt(product_id)];
+    
+    $(this).closest('.row').find('#product_price').val(product_price)
+    console.log(product_price)
+});
+
