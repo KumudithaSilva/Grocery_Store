955f7d78d5e2706b9d1f8667b1af24a97a746147
kumuditha <kumudithasilva66@gmail.com.>
2023-12-21 22:15:07 +0530

    create the full total of items

--- Grocery_Store/FrontEnd/js/order.js
+++ Grocery_Store/FrontEnd/js/order.js
@@ -4,23 +4,25 @@ $(function(){
     $.get(productListApiUrl, function(response){
         if(response){
             products_prices = {};
-            var options = '<option value="">--Select--</options>';
+            var options = '<option value="">--Select--</option>';
             
             $.each(response, function(index, products){
                 options += '<option value="'+ products.products_id + '">'+ products.name + '</option>';
                 products_prices[products.products_id] = products.price_per_unit;
             });
         }
-
         $('.product-box').find("select").empty().html(options);
     });
 });
 
 
-function fetchDetails(){
+function fetchAllDetails() {
     var formData = $("form").serializeArray();
     var products_details = [];
     var products_qty = [];
+
+    var products_alldetails = {};
+    var total = 0;
 
     $("select[name='product'] option:selected").each(function() {
         var productName = $(this).text().trim();
@@ -30,10 +32,17 @@ function fetchDetails(){
         }
     });
 
-    for(var i=0; i<formData.length; i++){
+    for (var i = 0; i < formData.length; i++) {
         var element = formData[i];
 
-        switch(element.name){
+        switch (element.name) {
+            case "product":
+                var product = element.value;
+                break;
+            case "item_total":
+                var item_total = element.value;
+                products_alldetails[product] = item_total;
+                break;
             case "qty":
                 var quantity = element.value;
                 products_qty.push(quantity);
@@ -41,7 +50,13 @@ function fetchDetails(){
         }
     }
 
+    for (var productId in products_alldetails) {
+        if (products_alldetails.hasOwnProperty(productId)) {
+            total += parseFloat(products_alldetails[productId]);
+        }
+    }
     var details = products_details.map((value, index) => ({ product: value, quantity: products_qty[index] }));
+    $('#product_grand_total').val(parseFloat(total).toFixed(2));
     return details;
 }
 
@@ -60,14 +75,17 @@ $('#addMoreButton').click(function(){
 
 $(document).on("click", '.remove-row', function(){
     $(this).closest('.row').remove();
+    fetchAllDetails();
 });
 
 
 $(document).on("change", ".cart-product", function(){
     var product_id = $(this).val();
     var product_price = products_prices[parseInt(product_id)];
+
     $(this).closest('.row').find('#product_price').val(product_price);
 
+    fetchAllDetails();
 });
 
 
@@ -76,13 +94,14 @@ $(document).on("change", ".row", function(){
     var quantity = $(this).closest('.row').find('.product-qty').val();
 
     item_total = (price * quantity).toFixed(2);
-
     $(this).closest('.row').find('#item_total').val(item_total);
+
+    fetchAllDetails();
 });
 
 
 orderModal.on('show.bs.modal', function(){
-    var detailsArray = fetchDetails();
+    var detailsArray = fetchAllDetails();
 
     var tableBody = $('#orderTable tbody');
     tableBody.empty();
@@ -112,8 +131,3 @@ $('#orderPrint').on("click", function(){
 
     html2pdf().from(table).set(opt).save();
 });
-
-
-
-
-
